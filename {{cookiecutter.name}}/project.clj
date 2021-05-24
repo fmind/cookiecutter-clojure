ddec3628d90fcf9943a860731879a9a62991fbd3
Médéric Hurier <3929438+fmind@users.noreply.github.com>
2021-05-24 20:46:30 +0200

    Update project.clj

--- {{cookiecutter.name}}/project.clj
+++ {{cookiecutter.name}}/project.clj
@@ -1,24 +1,19 @@
 (defproject {{cookiecutter.name}} "0.1.0-SNAPSHOT"
   :description "TODO: write a description"
-
   :url "{{cookiecutter.repository}}"
   :scm {:name "GIT" :url "{{cookiecutter.repository}}"}
   :license {:name "{{cookiecutter.license}}" :url "https://opensource.org/licenses/{{cookiecutter.license}}"}
-
   :test-paths ["test"]
   :source-paths ["src"]
   :resource-paths ["res"]
-
   :main ^:skip-aot {{cookiecutter.name}}.core
-
   :plugins [[lein-codox "0.10.7"]
-            [lein-cljfmt "0.6.4"]
-            [lein-binplus "0.6.5"]
-            [lein-ancient "0.6.15"]
+            [lein-cljfmt "0.7.0"]
+            [lein-binplus "0.6.6"]
+            [lein-ancient "0.7.0"]
             [lein-githooks "0.1.0"]
-            [lein-cloverage "1.1.1"]
-            [jonase/eastwood "0.3.5"]]
-
+            [lein-cloverage "1.2.2"]
+            [jonase/eastwood "0.4.3"]]
   :profiles {:dev {:githooks
                    {:pre-commit
                     ["lein test"
@@ -29,5 +24,4 @@
                      "lein codox"
                      "lein bin"]}}
              :uberjar {:aot :all}}
-
   :dependencies [[org.clojure/clojure "{{cookiecutter.clojure}}"]])
